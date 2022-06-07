2b0e6b6ec5a498838d003814a6a24b8ca91642ef
Denys-Saveliev <90144527+Denys-Saveliev@users.noreply.github.com>
2022-06-07 15:08:00 +0300

    Update App.jsx
    Adding lifeCycle metods for updating localeStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,19 @@ import ContactList from './ContactList';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleSearchContact = e => {
     this.setState({ filter: e.currentTarget.value });
@@ -23,8 +28,9 @@ class App extends Component {
 
   filterContactList = () => {
     const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
@@ -49,20 +55,22 @@ class App extends Component {
   };
 
   render() {
-    const { filter } = this.state;
+    const { filter, contacts } = this.state;
     return (
       <Container>
         <Section title="Phonebook">
           <ContactForm onSubmit={this.formSubmitHandler} />
         </Section>
 
-        <Section title="Contacts">
-          <Filter value={filter} onSearch={this.handleSearchContact} />
-          <ContactList
-            contacts={this.filterContactList()}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
+        {contacts.length > 0 && (
+          <Section title="Contacts">
+            <Filter value={filter} onSearch={this.handleSearchContact} />
+            <ContactList
+              contacts={this.filterContactList()}
+              onDeleteContact={this.deleteContact}
+            />
+          </Section>
+        )}
       </Container>
     );
   }
